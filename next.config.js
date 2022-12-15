103f8f6cf5cf33ef8d0002c73a1145983e9b2883
Aaron Vasilev <oigen711@gmail.com>
2022-12-15 16:47:00 +0200

    Added redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
